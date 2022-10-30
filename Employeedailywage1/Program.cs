212b3e974214f4f5fc67c008ee66372278fdeeb4
anushkachouhan029 <anushkachouhan029>
2022-10-30 22:43:43 +0500

    added employee wage7

--- Employeedailywage1/Program.cs
+++ Employeedailywage1/Program.cs
@@ -2,15 +2,19 @@
     public const int IS_PART_TIME = 1;
     public const int IS_FULL_TIME = 2;
     public const int EMP_RATE_PER_HOUR = 20;
+    public const int NUM_OF_WORKING_DAYS = 2;
+    public const int MAX_HRS_IN_MONTH = 10;
 
-    static void Main(string[]args)
+    public static int computeEmpWage()
     { 
 
     int empHrs = 0;
-    int empWage = 0;
+    int totalEmphrs = 0;
+    int totalworkingdays =0 ;
+    while (totalEmphrs < = MAX_HRS_IN_MONTH && totalworkingdays < NUM_OF_WORKING_DAYS)
+    { 
+    totalworkingdays++;
     Random random = new Random();
-
-    int empCheck = random.Next(0, 2);
     switch (empCheck)
     {
         case IS_PART_TIME;
@@ -23,7 +27,10 @@
          empHrs = 0;
          break;
     }
-     empWage = empHrs * EMP_RATE_PER_HOUR;
-     Console.WriteLine("Emp Wage :" + empWage);
-   }
+    totalEmphrs += empHrs;
+    Console.WriteLine("Day#:" + totalworkingdays + "emp hrs :" + empHrs);
+    }
+    int totalEmpWage = totalEmphrs * EMP_RATE_HER_HOUR;
+    Console.WriteLine("Total Emp Wage: " + totalEmpWage);
  }
+}
