ea7d8c05dfea478d79c11136e3ceed87c76dd032
anushkachouhan029 <anushkachouhan029>
2022-10-30 21:51:30 +0500

    add uc4employeewage1

--- Employeedailywage1/Program.cs
+++ Employeedailywage1/Program.cs
@@ -1,19 +1,29 @@
 ﻿class Program
-{
-    static void Main(String[] args) 
+    public const int IS_PART_TIME = 1;
+    public const int IS_FULL_TIME = 2;
+    public const int EMP_RATE_PER_HOUR = 20;
+
+    static void Main(string[]args)
     { 
 
-    int IS_FULL_TIME = 1;
+    int empHrs = 0;
+    int empWage = 0;
     Random random = new Random();
 
-        int empCheck = random.Next(0, 2);
-    if (empCheck == IS_FULL_TIME)
+    int empCheck = random.Next(0, 2);
+    switch (empCheck)
     {
-        Console.WriteLine("Employee is present");
+        case IS_PART_TIME;
+        empHrs = 4;
+        break;
+        case Is_Full_Time;
+        empHrs = 8;
+        break;
+        default:
+         empHrs = 0;
+         break;
     }
-    else
-    {
-    Console.WriteLine("Employee is not present");
-     }
+     empWage = empHrs * EMP_RATE_PER_HOUR;
+     Console.WriteLine("Emp Wage :" + empWage);
    }
  }
